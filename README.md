b5e19b5ce4c5eb08c701518ac15fb7c17ae89334
kharry06 <harringtonkyle68@gmail.com>
2025-12-01 01:06:52 -0500

    Instuctions and Rename

---
+++ README.md
@@ -0,0 +1,5 @@
+Right click on play.py and press "Run Python File in Terminal"
+
+OR
+
+Type in Terminal "uv run play.py"
---
+++ play.py
@@ -0,0 +1,83 @@
+from constants import *
+from logger import log_state, log_event
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from player import Player
+from shot import Shot
+import pygame
+import sys
+import random
+
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.vernum}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #initialize game
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #create clock
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    #containers
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    #initialize objects
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    #infinite update loop
+    while True:
+        log_state()
+
+        # 1. Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # 2. Update player
+        updatable.update(dt)
+
+        # 3. collision checks
+        for asteroid in asteroids:
+            if asteroid.collides_with(player) == True:
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
+
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot) == True:
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+
+
+        # 4. Drawing
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+        
+        pygame.display.flip()
+
+        # 5. Tick for 60 FPS
+        dt = clock.tick(60) / 1000
+
+
+
+if __name__ == "__main__":
+    main()
